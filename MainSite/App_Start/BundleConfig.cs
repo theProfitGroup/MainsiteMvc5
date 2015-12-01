4c88e8eb55447f564d9e3c98668d0bdf2416e963
igor.prokofjev <igor.prokofjev@groupdocs.com>
2015-12-01 19:42:12 +0200

    Create asp.net mvc 4 application;

---
+++ MainSite/App_Start/BundleConfig.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Optimization;
+
+namespace MainSite.App_Start
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                        "~/Scripts/jquery-1.10.2.min.js",
+                        "~/Scripts/jquery.easing.1.3.js",
+                        "~/Scripts/jquery.flexslider.js",
+                        "~/Scripts/bootstrap.min.js",
+                        "~/Scripts/default.js"
+                        ));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Scripts/modernizr-*"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(                                
+                "~/Content/css/bootstrap.min.css",
+                "~/Content/css/style.css",
+                "~/Content/css/flexslider.css"
+                ));
+        }
+    }
+}

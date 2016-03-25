d8aee753b99229470520cb30db3d1892457d946e
IgorGolovko <igor.golovko.developer@gmail.com>
2016-03-25 22:26:28 +0200

    Add view, controllers and etc.

--- MainSite/App_Start/RouteConfig.cs
+++ MainSite/App_Start/RouteConfig.cs
@@ -12,6 +12,12 @@ namespace MainSite
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("images/{*pathInfo}");
+            routes.IgnoreRoute("{resource}.ico/{*pathInfo}");
+
+            
+
+            
 
             routes.MapRoute(
                 name: "Default",
--- MainSite/Controllers/HomeController.cs
+++ MainSite/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MainSite.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,7 +7,7 @@ using System.Web.Mvc;
 
 namespace MainSite.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         //
         // GET: /Home/

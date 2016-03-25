f2ffc214fd76792e7227a15ade9a99c1f31f1e85
IgorGolovko <igor.golovko.developer@gmail.com>
2016-03-25 22:32:51 +0200

    added missingcontrollers

---
+++ MainSite/App_Start/ApplicationConfig.cs
@@ -0,0 +1,46 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ApplicationConfig.cs" company="Company name">
+//   -
+// </copyright>
+// <author>Igor Golovko</author>
+// <summary>
+//   Summary description for ApplicationConfig
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace MainSite
+{
+    using System.Web.Http;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Serialization;
+    /// <summary>
+    /// Application settings class.
+    /// </summary>
+    public class ApplicationConfig
+    {
+        /// <summary>
+        /// The register settings.
+        /// </summary>
+        /// <param name="configuration">
+        /// The configuration.
+        /// </param>
+        public static void RegisterSettings(HttpConfiguration configuration)
+        {
+            // Web API routes
+            //configuration.MapHttpAttributeRoutes();
+
+            // Convert all dates to UTC
+            configuration.Formatters.JsonFormatter.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Utc;
+
+            // write indented JSON
+            configuration.Formatters.JsonFormatter.SerializerSettings.Formatting = Formatting.Indented;
+
+            // write JSON property names with camel casing
+            configuration.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            // preserve object references in JSON
+            configuration.Formatters.JsonFormatter.SerializerSettings.PreserveReferencesHandling = PreserveReferencesHandling.All;
+        }
+    
+    }
+}
---
+++ MainSite/Controllers/WorksController.cs
@@ -0,0 +1,21 @@
+﻿using MainSite.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MainSite.Controllers
+{
+    public class WorksController : BaseController
+    {
+        //
+        // GET: /Works/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}

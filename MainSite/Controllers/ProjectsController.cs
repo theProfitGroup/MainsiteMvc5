31c8785aa46cdb45f3501f5c55fc084596f3eb6d
igor.prokofjev <igor.prokofjev@groupdocs.com>
2016-03-28 18:38:46 +0300

    add  missing files

---
+++ MainSite/Controllers/ProjectsController.cs
@@ -0,0 +1,16 @@
+﻿using MainSite.Core;
+using System.Web.Mvc;
+
+namespace MainSite.Controllers
+{
+    public class ProjectsController : BaseController
+    {
+        //
+        // GET: /Works/
+
+        public ActionResult Index()
+        {
+            return View("~/Views/Home/Projects.cshtml");
+        }
+    }
+}
